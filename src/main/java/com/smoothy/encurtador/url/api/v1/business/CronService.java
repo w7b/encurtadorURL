989dfcedb2799aee73c88a829ab7f10e013a593b
w7b <pessoal.gabriel21@gmail.com>
2025-02-16 19:06:41 -0300

    Optimized code and arch

--- src/main/java/com/smoothy/encurtador/url/api/v1/business/CronService.java
+++ src/main/java/com/smoothy/encurtador/url/api/v1/business/CronService.java
@@ -1,12 +1,14 @@
-package com.smoothy.encurtador.url.business;
+package com.smoothy.encurtador.url.api.v1.business;
 
-import com.smoothy.encurtador.url.business.dto.out.UrlDtoResponse;
-import com.smoothy.encurtador.url.business.enums.UrlStats;
+import com.smoothy.encurtador.url.api.v1.dto.out.UrlDtoResponse;
+import com.smoothy.encurtador.url.api.v1.enums.UrlStats;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.scheduling.annotation.Scheduled;
 import org.springframework.stereotype.Service;
 
+import java.time.Instant;
 import java.time.LocalDateTime;
 import java.util.List;
 
@@ -15,19 +17,20 @@ import java.util.List;
 @Slf4j
 public class CronService {
 
+    @Autowired
     private final UrlService urlService;
 
-    @Scheduled(cron = "${cron.hour}")
+    @Scheduled(cron = "${cron-time}")
     public void findUrlByExpiredTime() {
         LocalDateTime createAt = LocalDateTime.now();
-        LocalDateTime timeExpiration = LocalDateTime.now().plusWeeks(2);
+        LocalDateTime timeExpiration = LocalDateTime.now().plusMinutes(15);
 
-        List<UrlDtoResponse> urlList = urlService.buscaUrlExpiradaPeriodo(createAt, timeExpiration);
+        List<UrlDtoResponse> urlList = urlService.findUrlExpiradaPeriodo(createAt, timeExpiration);
         log.info("Lista de Urls encontradas " +urlList);
 
         urlList.forEach(url -> {
-            urlService.buscaUrlExpiradaPorId(url.getId());
-            UrlDtoResponse urlStatusUpdater = urlService.updateStatus(UrlStats.EXPIRED, url.getId());
+            urlService.findUrlExpiradaPorId(url.getId());
+            UrlDtoResponse urlStatusUpdater = urlService.statsUpdate(UrlStats.EXPIRED, url.getId());
             log.info("URL's " +url.getOUrl()+ "has defined to " +urlStatusUpdater);
             urlService.deleteById(url.getId());
             log.info("A URL encurtada foi deletada com sucesso da nossa DataBase");
---
+++ src/main/java/com/smoothy/encurtador/url/api/v1/enums/UrlStats.java
@@ -0,0 +1,5 @@
+package com.smoothy.encurtador.url.api.v1.enums;
+
+public enum UrlStats {
+    EXPIRED, ALIVE
+}
